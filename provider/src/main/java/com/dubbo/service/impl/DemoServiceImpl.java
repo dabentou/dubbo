fcfee3e53000f4d8c29c8b7995d64ad28fcfdb21
Administrator <513213884@qq.com>
2016-12-29 19:10:34 +0800

    modify

--- provider/src/main/java/com/dubbo/service/impl/DemoServiceImpl.java
+++ provider/src/main/java/com/dubbo/service/impl/DemoServiceImpl.java
@@ -1,7 +1,21 @@
 package com.dubbo.service.impl;
 
+import com.dubbo.bean.User;
+import com.dubbo.service.DemoService;
+
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/12/29.
  */
-public class DemoServiceImpl {
+public class DemoServiceImpl implements DemoService{
+
+
+    public String sayHello(String name) {
+        return null;
+    }
+
+    public List<User> getUsers() {
+        return null;
+    }
 }
