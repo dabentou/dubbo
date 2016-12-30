17336d577b66eb07f60c62c621e0fc1a2c9aa013
Administrator <513213884@qq.com>
2016-12-30 18:32:18 +0800

    现价客户端依赖包

---
+++ provider/src/main/java/com/dubbo/service/UserService.java
@@ -0,0 +1,9 @@
+package com.dubbo.service;
+
+/**
+ * Created by Administrator on 2016/12/30.
+ */
+public interface UserService {
+
+    public int getAget();
+}
---
+++ provider/src/main/java/com/dubbo/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.dubbo.service.impl;
+
+import com.dubbo.service.UserService;
+
+/**
+ * Created by Administrator on 2016/12/30.
+ */
+public class UserServiceImpl implements UserService {
+    public int getAget() {
+        return 0;
+    }
+}
